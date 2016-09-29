1ee32d62b7e1bf1ee358d8d63f59c17f2fe3161b
leonardo.yvens <leoyvens@gmail.com>
2016-09-29 16:52:34 -0300

    initial commit

---
+++ spark.py
@@ -0,0 +1,36 @@
+from pyspark import SparkContext, SparkConf
+
+def get_author(entry):
+    for line in entry[1].splitlines():
+        if line.startswith("Author: "):
+            author = line[8:].strip()
+    return (author, entry[1])
+
+def word_count(text):
+    wordcount={}
+    for word in text.split():
+        if len(word) < 4:
+            continue
+        if word not in wordcount:
+            wordcount[word] = 1
+        else:
+            wordcount[word] += 1
+    return wordcount
+
+def merge(dict1, dict2):
+    # Merge dict2 into dict1
+    for key in dict2.keys():
+        if key in dict1:
+            dict1[key] += dict2[key]
+        else:
+            dict1[key] = dict2[key]
+    return dict1
+
+def sort_dict(dict):
+    return sorted(dict.items(), key=lambda x: x[1], reverse=True)
+
+sc = SparkContext()
+rdd = sc.wholeTextFiles("dataset")
+rdd = rdd.map(get_author).mapValues(word_count).reduceByKey(merge)
+rdd = rdd.mapValues(sort_dict).mapValues(lambda x: x[0:5])
+print(rdd.saveAsTextFile("output"))
